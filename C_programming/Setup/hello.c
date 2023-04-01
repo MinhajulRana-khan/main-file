7ffee0f11a3787d19aeaefcb59a2c24e05a9bd8a
MinhajulRana-Khan <minhajulislam017596@gmail.com>
2023-04-02 01:22:05 +0600

    something added

--- C_programming/Setup/hello.c
+++ C_programming/Setup/hello.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
-    printf("Hello! world>> I am coming towards you be ready");
 
+    printf("Hello! world>> I am coming towards you be ready");
+    printf("I am Minhajul Islam");
     return 0;
 }
